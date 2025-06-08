54d86b38c394f81299bbf4eaa81286686031652b
Shaheem Javied <sjav200@outlook.com>
2025-06-08 01:28:19 -0400

    Add files via upload

--- resource/global.js
+++ resource/global.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     var contextmenu = document.getElementById("contextnavmenu");
     var contextmenubtn = document.getElementById("contextmenubtn");
+    var screendim = document.getElementById("dimscreen");
 
-    if (contextmenubtn) { // Check if element exists
+    if (contextmenubtn) {
         contextmenubtn.addEventListener('click', function() {
             if (contextmenu.style.display === "none") {
                 contextmenu.style.display = "block";
+                screendim.style.display = "block";
             } else {
                 contextmenu.style.display = "none";
+                screendim.style.display = "none";
+            }
+        });
+
+        document.getElementById("dimscreen").addEventListener("click", function() {
+            if (window.getComputedStyle(screendim).display === "block") {
+                contextmenu.style.display = "none";
+                screendim.style.display = "none";
             }
         });
     }
